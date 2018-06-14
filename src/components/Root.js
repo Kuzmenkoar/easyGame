6f32301cc1e277153614e9f13f4d4bf2d40222da
kuzmenkoar <kuzmenko.vyshneve@gmail.com>
2018-06-14 17:57:29 +0300

    Initial commit
    First commit to master
    
    master -> master

---
+++ src/components/Root.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+// import {Route} from 'react-router-dom'
+
+// import {Link} from 'react-router-dom'
+
+class Root extends Component {
+
+    render() {
+        return (
+          <div>
+              Hello world!
+          </div>  
+        )
+    }
+}
+
+export default Root
